0a20b2c9cf8812d3199240856b1ab5cb587dfeb5
Alex Smith <alexsmith716@outlook.com>
2018-04-19 15:58:45 -0400

    Added content to component  ContentContainer.

--- src/components/ContentContainer/ContentContainer.js
+++ src/components/ContentContainer/ContentContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from './ContentContainer.scss';
+import styles from './ContentContainer2.scss';
 
 export default class ContentContainer extends React.Component {
 
@@ -8,18 +8,44 @@ export default class ContentContainer extends React.Component {
 
     return (
 
-      <div>
-        <hr/>
-        <hr/>
-        <hr/>
-        <h1>Home !!!!!!</h1>
-        <hr/>
-        <hr/>
-        <hr/>
-      </div>
+    <div>
+      <main role="main">
+
+        <div className={`jumbotron ${styles.masthead} d-flex`}>
+          <div className="container">
+            <h1 className="display-3">Isomorphic App!</h1>
+            <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+            <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+          </div>
+        </div>
+
+        <div className="container">
+
+          <div className="row">
+            <div className="col-md-4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+              <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+            <div className="col-md-4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.</p>
+              <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+            <div className="col-md-4">
+              <h2>Heading</h2>
+              <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper.Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+              <p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
+            </div>
+          </div>
+
+          <hr/>
+
+        </div>
+
+      </main>
+    </div>
 
     )
-
   };
-
 };
